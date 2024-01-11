97cf9e887861343bee51a4c86734d2f8c6bf8c7e
Gabrielli de Lourdes Garcia <107142414+GabiL-Garcia@users.noreply.github.com>
2024-01-11 17:33:59 -0300

    Add files via upload

---
+++ ExercicioFuncionario/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioFuncionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroFuncionario, horasTrabalhadas;
+            double salario, calculoSalario;
+
+            numeroFuncionario = int.Parse(Console.ReadLine());
+            horasTrabalhadas = int.Parse(Console.ReadLine());
+
+            salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            calculoSalario = (double)salario * horasTrabalhadas;
+
+            Console.WriteLine("NUMBER = " + numeroFuncionario);
+            Console.WriteLine("SALARY = U$ " + calculoSalario.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
