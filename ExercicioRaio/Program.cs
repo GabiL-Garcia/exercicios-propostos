aa959e8be894e98d22468ba4b868115d1a215d4c
Gabrielli de Lourdes Garcia <107142414+GabiL-Garcia@users.noreply.github.com>
2024-01-11 17:06:41 -0300

    Add files via upload

---
+++ ExercicioRaio/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioRaio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio, pi, formulaArea;
+
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            pi = 3.14159;
+            formulaArea = pi * Math.Pow(raio, 2);
+
+            Console.WriteLine("A=" + formulaArea.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
