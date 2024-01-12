4f091d2d1530c63ebfc0fa46c8ea1ef2b1820900
Gabrielli de Lourdes Garcia <107142414+GabiL-Garcia@users.noreply.github.com>
2024-01-12 15:23:09 -0300

    Add files via upload

---
+++ ExercicioMultiplos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExercicioMultiplos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vet = Console.ReadLine().Split(' ');
+            int a = int.Parse(vet[0]);
+            int b = int.Parse(vet[1]);
+
+            if (a % b == 0 || b % a == 0)
+            {
+                Console.WriteLine("São múltiplos");
+            }
+            else
+            {
+                Console.WriteLine("Não são múltiplos");
+            }
+          
+        }
+    }
+}
