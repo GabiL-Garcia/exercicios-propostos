fad48f37f6eb3fc5c496855e4edea67a5fad20e7
Gabrielli de Lourdes Garcia <107142414+GabiL-Garcia@users.noreply.github.com>
2024-01-15 15:53:41 -0300

    Add files via upload

---
+++ ExercicioSenha/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExercicioSenha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int senha = 2002;
+            int tentativaSenha = int.Parse(Console.ReadLine());
+
+            while (tentativaSenha != senha)
+            {
+                Console.WriteLine("Senha invalida");
+                tentativaSenha = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Acesso permitido");
+        }
+    }
+}
