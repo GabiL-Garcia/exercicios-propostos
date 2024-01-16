1c97ef03fb4291270111a598dbe5652f7eff8b13
Gabrielli de Lourdes Garcia <107142414+GabiL-Garcia@users.noreply.github.com>
2024-01-16 14:39:24 -0300

    Add files via upload

---
+++ ExercicioDivisao/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioDivisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            {
+                string[] l = Console.ReadLine().Split(' ');
+                int l1 = int.Parse(l[0]);
+                int l2 = int.Parse(l[1]);
+
+                if(l2 == 0)
+                {
+                    Console.WriteLine("divisao impossivel");
+                }
+                else
+                {
+                    double d = (double)l1 / l2;
+                    Console.WriteLine(d.ToString("F1", CultureInfo.InvariantCulture));
+                }
+
+        
+            }
+        }
+    }
+}
