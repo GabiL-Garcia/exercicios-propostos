5ee2b42ad6d84cc6f71adf625dc97d4490fd6ed7
Gabrielli de Lourdes Garcia <107142414+GabiL-Garcia@users.noreply.github.com>
2024-01-16 15:15:42 -0300

    Add files via upload

---
+++ ExercicioLinhas/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExercicioLinhas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            {
+                int _1 = i;
+                int _2 = i * i;
+                int _3 = i * i * i;
+
+                Console.WriteLine("{0} {1} {2}" ,_1 , _2, _3);
+            }
+        }
+    }
+}
