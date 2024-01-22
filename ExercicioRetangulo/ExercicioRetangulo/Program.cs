bf9a8691455453c4fa134679fc8e9b4577b7e5ce
Gabrielli de Lourdes Garcia <107142414+GabiL-Garcia@users.noreply.github.com>
2024-01-22 15:31:25 -0300

    Add files via upload

---
+++ ExercicioRetangulo/ExercicioRetangulo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioRetangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo t = new Retangulo();
+
+            Console.WriteLine("Entre a largura e a altura do retângulo:");
+            t.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            t.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("AREA = " + t.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERÍMETRO = " + t.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + t.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
