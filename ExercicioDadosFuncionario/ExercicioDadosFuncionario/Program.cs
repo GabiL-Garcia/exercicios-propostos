fb56c93cb1ba857e84f7beb401747dfa8a590469
Gabrielli de Lourdes Garcia <107142414+GabiL-Garcia@users.noreply.github.com>
2024-01-22 15:51:22 -0300

    Add files via upload

---
+++ ExercicioDadosFuncionario/ExercicioDadosFuncionario/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioDadosFuncionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Funcionario f = new Funcionario();
+
+            Console.Write("Nome: ");
+            f.Nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            f.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            f.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Funcionário: " + f);
+
+            Console.WriteLine();
+
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double p = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            f.AumentarSalario(p);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Dados atualizados: "  + f);
+
+        }
+    }
+}
