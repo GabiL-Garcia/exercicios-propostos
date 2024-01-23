0d3fbfd3d47614e6e837c7f0b2e43be4a686ee30
Gabrielli de Lourdes Garcia <107142414+GabiL-Garcia@users.noreply.github.com>
2024-01-23 16:56:05 -0300

    Add files via upload

---
+++ ExercicioDolar/ExercicioDolar/ConversorDeMoeda.cs
@@ -0,0 +1,16 @@
+﻿namespace ExercicioDolar
+{
+    class ConversorDeMoeda
+    {
+        
+        public static double Iof = 6.0;
+
+        public static double Dolar(double cotacao, double compra)
+        {
+
+            double total = cotacao * compra;
+            return total + total * Iof / 100;
+        }
+
+    }
+}
